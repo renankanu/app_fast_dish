7d6585937569d6fc430fcd21860c09724685346d
renankanu <renankanu@gmail.com>
2020-10-27 20:58:43 -0300

    Feature - create Intro Screen

--- lib/app/modules/intro_page/intro_page.dart
+++ lib/app/modules/intro_page/intro_page.dart
@@ -1,7 +1,20 @@
 import 'package:app_fast_dish/app/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
