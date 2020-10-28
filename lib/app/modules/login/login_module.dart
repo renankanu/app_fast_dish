7c78b1eeb7197f7860ab941c8d01ef2e0343114a
renankanu <renankanu@gmail.com>
2020-10-27 21:15:26 -0300

    Feature - create login module

---
+++ lib/app/modules/login/login_module.dart
@@ -0,0 +1,21 @@
+import 'login_controller.dart';
+import 'package:dio/dio.dart';
+import 'repositories/login_repository.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'login_page.dart';
+
+class LoginModule extends ChildModule {
+  @override
+  List<Bind> get binds => [
+        $LoginController,
+        $LoginRepository,
+      ];
+
+  @override
+  List<ModularRouter> get routers => [
+        ModularRouter(Modular.initialRoute, child: (_, args) => LoginPage()),
+      ];
+
+  static Inject get to => Inject<LoginModule>.of();
+}
